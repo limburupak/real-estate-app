ac82d68c3f4ed847d94ecae89d740169b969d940
limburupak <rupak@nativenotify.com>
2026-06-11 14:23:48 +0530

    Fix plugin: better approach for adding pickFirst to build.gradle

--- plugins/withAndroidPickFirst.js
+++ plugins/withAndroidPickFirst.js
@@ -1,22 +1,40 @@
-const { withProjectBuildGradle } = require('@expo/config-plugins');
+cat > plugins/withAndroidPickFirst.js << 'EOF'
+const { withBuildGradle } = require('@expo/config-plugins');
 
 module.exports = function withAndroidPickFirst(config) {
-  return withProjectBuildGradle(config, (config) => {
+  return withBuildGradle(config, (config) => {
     if (config.modResults.language === 'groovy') {
-      if (!config.modResults.contents.includes('pickFirst \'META-INF/versions/9/OSGI-INF/MANIFEST.MF\'')) {
-        if (config.modResults.contents.includes('packagingOptions {')) {
-          config.modResults.contents = config.modResults.contents.replace(
-            'packagingOptions {',
-            'packagingOptions {\n        pickFirst \'META-INF/versions/9/OSGI-INF/MANIFEST.MF\''
-          );
-        } else {
-          config.modResults.contents = config.modResults.contents.replace(
-            'androidResources {',
-            'packagingOptions {\n        pickFirst \'META-INF/versions/9/OSGI-INF/MANIFEST.MF\'\n    }\n\n    androidResources {'
-          );
+      // Add the pickFirst line at the end of the android block
+      const pickFirstLine = '        pickFirst \'META-INF/versions/9/OSGI-INF/MANIFEST.MF\'';
+      
+      // Check if already added
+      if (!config.modResults.contents.includes(pickFirstLine)) {
+        // Find the android block and add pickFirst inside packagingOptions
+        const androidBlockMatch = config.modResults.contents.match(/android\s*\{([^]*?)\n\s*\}/);
+        
+        if (androidBlockMatch) {
+          const androidBlock = androidBlockMatch[0];
+          
+          // Check if packagingOptions exists
+          if (androidBlock.includes('packagingOptions {')) {
+            // Add pickFirst inside existing packagingOptions
+            const newAndroidBlock = androidBlock.replace(
+              /(packagingOptions\s*\{[^]*?)(\n\s*\})/,
+              `$1\n${pickFirstLine}$2`
+            );
+            config.modResults.contents = config.modResults.contents.replace(androidBlock, newAndroidBlock);
+          } else {
+            // Add packagingOptions block before the closing brace of android
+            const newAndroidBlock = androidBlock.replace(
+              /(\n\s*\})$/,
+              `\n    packagingOptions {\n${pickFirstLine}\n    }\n$1`
+            );
+            config.modResults.contents = config.modResults.contents.replace(androidBlock, newAndroidBlock);
+          }
         }
       }
     }
     return config;
   });
 };
+EOF
